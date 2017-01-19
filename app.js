099aed0cf0cb06a3c4c9ee370294e15fdfee4f37
Jon <jon.bloomer@iag.com.au>
2017-01-19 13:52:15 +1100

    updated express all route to preload globals and brands data from cosmic

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
-var express = require('express');
-var fs = require('fs');
-var app = express();
-var request = require('superagent');
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const request = require('superagent');
 
-var API_BASEURL = 'https://api.cosmicjs.com/v1/tonetemple/';
-var API_READ_KEY = 'A5Svxw4tq11rLGxnCGx7MLOUA7QIiznqjANMeqewHCsH7jX1vd'; //read_key=
-var GLOBALS = API_BASEURL + 'object-type/globals?read_key=' + API_READ_KEY + '&hide_metafields=true';
-var BRANDS = API_BASEURL + 'object-type/brands?read_key=' + API_READ_KEY + '&hide_metafields=true';
-
+const API_BASEURL = 'https://api.cosmicjs.com/v1/tonetemple/';
+const API_READ_KEY = 'A5Svxw4tq11rLGxnCGx7MLOUA7QIiznqjANMeqewHCsH7jX1vd'; //read_key=
+const GLOBALS = `${API_BASEURL}object-type/globals?read_key=${API_READ_KEY}&hide_metafields=true`;
+const BRANDS = `${API_BASEURL}object-type/brands?read_key=${API_READ_KEY}&hide_metafields=true`;
 
 app.use('/public', express.static(__dirname + '/public'));
 
 app.locals = {
-    title: 'express example'
+    title: 'Tone Temple - Exclusive Strandberg Australia Retailers'
 };
 
 app.all('*', function(req, res, next) {
     request
         .get(GLOBALS)
         .end(function(err, res) {
-            console.log('end');
-            console.log(res);
-        })
+            console.log('fetch globals');
+            app.locals.globals = JSON.parse(res.text);
+            // console.log(app.locals.globals.objects);
+            app.locals.globals.objects.forEach(global => {
+                console.log(global);
+            })
+        });
+    request
+        .get(BRANDS)
+        .end(function(err, res) {
+            console.log('fetch brands');
+            app.locals.brands = JSON.parse(res.text);
+            console.log(app.locals.brands);
+            app.locals.brands.objects.forEach(brand => {
+                console.log(brand);
+            });
+        });
     fs.readFile('posts.json', function(err, data) {
         res.locals.posts = JSON.parse(data);
         next();
     })
-})
+});
 
 app.get('/', function(req, res) {
     console.log('index page');
