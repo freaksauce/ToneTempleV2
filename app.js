89bbd907461cc3e3bd8a8ddbe9be2855307a99c0
Jon <jon.bloomer@iag.com.au>
2017-01-19 13:59:29 +1100

    reordered express routes and commented out old code

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const request = require('superagent');
 const API_BASEURL = 'https://api.cosmicjs.com/v1/tonetemple/';
 const API_READ_KEY = 'A5Svxw4tq11rLGxnCGx7MLOUA7QIiznqjANMeqewHCsH7jX1vd'; //read_key=
 const GLOBALS = `${API_BASEURL}object-type/globals?read_key=${API_READ_KEY}&hide_metafields=true`;
+// https://api.cosmicjs.com/v1/tonetemple/object-type/globals?pretty=true&hide_metafields=true&read_key=A5Svxw4tq11rLGxnCGx7MLOUA7QIiznqjANMeqewHCsH7jX1vd
 const BRANDS = `${API_BASEURL}object-type/brands?read_key=${API_READ_KEY}&hide_metafields=true`;
+// https://api.cosmicjs.com/v1/tonetemple/object-type/brands?pretty=true&hide_metafields=true&read_key=A5Svxw4tq11rLGxnCGx7MLOUA7QIiznqjANMeqewHCsH7jX1vd
 
 app.use('/public', express.static(__dirname + '/public'));
 
@@ -20,25 +22,19 @@ app.all('*', function(req, res, next) {
         .end(function(err, res) {
             console.log('fetch globals');
             app.locals.globals = JSON.parse(res.text);
-            // console.log(app.locals.globals.objects);
-            app.locals.globals.objects.forEach(global => {
-                console.log(global);
-            })
+            console.log(app.locals.globals.objects);
         });
     request
         .get(BRANDS)
         .end(function(err, res) {
             console.log('fetch brands');
             app.locals.brands = JSON.parse(res.text);
-            console.log(app.locals.brands);
-            app.locals.brands.objects.forEach(brand => {
-                console.log(brand);
-            });
+            console.log(app.locals.brands.objects);
         });
-    fs.readFile('posts.json', function(err, data) {
-        res.locals.posts = JSON.parse(data);
-        next();
-    })
+    // fs.readFile('posts.json', function(err, data) {
+    //     res.locals.posts = JSON.parse(data);
+    //     next();
+    // })
 });
 
 app.get('/', function(req, res) {
@@ -46,20 +42,12 @@ app.get('/', function(req, res) {
     res.render('index.ejs');
 });
 
-app.get('/post/:slug', function(req, res, next) {
-    res.locals.posts.forEach(function(post) {
-        if (req.params.slug === post.slug) {
-            res.render('post.ejs', { post: post });
-        }
-    });
-});
-
 app.get('/brand/:slug', function(req, res, next) {
-    res.locals.posts.forEach(function(post) {
-        if (req.params.slug === post.slug) {
+    res.locals.brands.objects.forEach(brand => {
+        if (req.params.slug === brand.slug) {
             res.render('brand.ejs', { brand: brand });
         }
-    });
+    })
 });
 
 app.get('/contact-us', function(req, res) {
@@ -67,9 +55,13 @@ app.get('/contact-us', function(req, res) {
     res.render('contactus.ejs');
 });
 
-app.get('/api/posts', function(req, res) {
-    res.json(res.locals.posts);
-});
+// app.get('/post/:slug', function(req, res, next) {
+//     res.locals.posts.forEach(function(post) {
+//         if (req.params.slug === post.slug) {
+//             res.render('post.ejs', { post: post });
+//         }
+//     });
+// });
 
 
 app.listen(3000);
