1177402c3dd50e0cf614c2df30a5403fd86e244b
Jon <jon.bloomer@iag.com.au>
2017-01-19 11:41:53 +1100

    added cosmicjs package and code stubs

--- app.js
+++ app.js
@@ -31,5 +31,51 @@ app.get('/api/posts', function(req, res) {
     res.json(res.locals.posts);
 });
 
+/**
+    CosmicJS start
+**/
+var config = {};
+config.bucket = {
+  slug: 'tonetemple',
+  read_key: 'A5Svxw4tq11rLGxnCGx7MLOUA7QIiznqjANMeqewHCsH7jX1vd',
+  write_key: '' // add write_key if added to your Cosmic JS bucket settings
+};
+var Cosmic = require('cosmicjs');
+
+/* Get bucket
+================================ */
+Cosmic.getBucket(config, function(error, response){
+  // console.log(response);
+});
+
+/* Get objects
+================================ */
+Cosmic.getObjects(config, function(error, response){
+  // console.log(response);
+});
+
+/* Get objects by type
+================================ */
+var params = {
+  type_slug: 'posts',
+  limit: 5,
+  skip: 0
+};
+Cosmic.getObjectType(config, params, function(error, response){
+  // console.log(response);
+});
+
+/* Get object
+================================ */
+var params = {
+  slug: 'object-slug'
+}
+Cosmic.getObject(config, params, function(error, response){
+  // console.log(response);
+});
+/**
+    CosmicJS end
+**/
+
 app.listen(3000);
 console.log('app is listening at localhost:3000');
