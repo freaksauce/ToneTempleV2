f947ec9038bec3a94f6f30b34759acb1a898933c
Jon <jon.bloomer@iag.com.au>
2017-01-19 16:56:01 +1100

    moved request so that data is avaiable at render time for meta tags:

--- app.js
+++ app.js
@@ -13,33 +13,42 @@ const BRANDS = `${API_BASEURL}object-type/brands?read_key=${API_READ_KEY}&hide_m
 app.use('/public', express.static(__dirname + '/public'));
 
 app.locals = {
-    title: 'Tone Temple - Exclusive Strandberg Australia Retailers'
+    title: 'Tone Temple - Exclusive Strandberg Australia Retailers',
+    metaDescription: 'Tone Temple is the exclusive Strandberg Australia Retailer as well as retailer for brands such as Friedman Amplifiers, Yankee Power Supplies and Evil Angel Pickups'
 };
 
 app.all('*', function(req, res, next) {
+    fs.readFile('posts.json', function(err, data) {
+        res.locals.posts = JSON.parse(data);
+        next();
+    })
+});
+
+app.get('/', function(req, res) {
+    const myRes = res;
     request
         .get(GLOBALS)
         .end(function(err, res) {
             console.log('fetch globals');
             app.locals.globals = JSON.parse(res.text);
-            console.log(app.locals.globals.objects);
+            // console.log(app.locals.globals.objects);
+            app.locals.globals.objects.forEach(global => {
+                if (global.slug === 'meta-description') {
+                    app.locals.metaDescription = stripTags(global.content);
+                    // console.log(app.locals.metaDescription, 'metaDescription');
+                }
+            });
+            // next();
+            console.log('index page metaDescription : ', app.locals.metaDescription);
+            myRes.render('index.ejs', { metaDescription: app.locals.metaDescription });
         });
     request
         .get(BRANDS)
         .end(function(err, res) {
             console.log('fetch brands');
             app.locals.brands = JSON.parse(res.text);
-            console.log(app.locals.brands.objects);
+            // console.log(app.locals.brands.objects);
         });
-    // fs.readFile('posts.json', function(err, data) {
-    //     res.locals.posts = JSON.parse(data);
-    //     next();
-    // })
-});
-
-app.get('/', function(req, res) {
-    console.log('index page');
-    res.render('index.ejs');
 });
 
 app.get('/brand/:slug', function(req, res, next) {
@@ -63,6 +72,9 @@ app.get('/contact-us', function(req, res) {
 //     });
 // });
 
+function stripTags(str) {
+    return str.replace(/(<([^>]+)>)/ig,"");
+}
 
 app.listen(3000);
 console.log('app is listening at localhost:3000');
